faf250d8d3267338a63623498fe267a61ef6c018
Bismark De Carvalho <bismarkcarvalho300@gmail.com>
2020-03-24 19:29:15 -0300

    Implementacao da lista de Departamento

--- src/gui/DepartmentListViewController.java
+++ src/gui/DepartmentListViewController.java
@@ -3,8 +3,11 @@ package gui;
 import static application.Main.getMainScene;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -13,9 +16,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListViewController implements Initializable {
 
+	private DepartmentService departmentService;
+	
 	@FXML
 	private Button btnNew;
 	
@@ -28,9 +34,17 @@ public class DepartmentListViewController implements Initializable {
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
 	
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtnNewAction() {
 		System.out.println("teste btn new");
+	}
+	
+	
+	
+	public void setDepartmentService(DepartmentService departmentService) {
+		this.departmentService = departmentService;
 	}
 	
 	
@@ -46,6 +60,18 @@ public class DepartmentListViewController implements Initializable {
 		
 		Stage stage = (Stage) getMainScene().getWindow();
 		tableViewDepartments.prefHeightProperty().bind(stage.heightProperty());
+		
+	}
+	
+	
+	public void updateTableView() {
+		if (departmentService == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		List<Department> list = departmentService.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartments.setItems(obsList);
 		
 	}
 
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,61 +15,81 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
-	
+
 	@FXML
 	public void onMenuItenSellerAction() {
 		System.out.println("onMenuItenSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentListView.fxml");
+		loadView2("/gui/DepartmentListView.fxml");
+
 	}
-	
-	
+
 	@FXML
-	public void onMenuItemAboutAction() { 
+	public void onMenuItemAboutAction() {
 		loadView("/gui/AboutView.fxml");
 	}
-	
+
 	private synchronized void loadView(String absolutename) {
 		try {
-			
+
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutename));
 			VBox newvbox = loader.load();
-			
+
 			Scene mainScene = Main.getMainScene();
-			VBox mainVbox =(VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newvbox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlerts("Io Exception", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private synchronized void loadView2(String absolutename) {
+		try {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutename));
+			VBox newvbox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
 			Node mainMenu = mainVbox.getChildren().get(0);
 			mainVbox.getChildren().clear();
 			mainVbox.getChildren().add(mainMenu);
 			mainVbox.getChildren().addAll(newvbox.getChildren());
 			
-			
+			DepartmentListViewController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+						
+
 		} catch (IOException e) {
 			Alerts.showAlerts("Io Exception", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+
 	}
 
-	
-	
 }
