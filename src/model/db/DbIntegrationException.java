09eab6ede88d6b207e5d42c21f96db8bab1db6de
Bismark De Carvalho <bismarkcarvalho300@gmail.com>
2020-03-27 09:23:26 -0300

    Integração com o banco de dados

---
+++ src/model/db/DbIntegrationException.java
@@ -0,0 +1,14 @@
+package model.db;
+
+public class DbIntegrationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DbIntegrationException(String msg) {
+		super(msg);
+	}
+	
+}
